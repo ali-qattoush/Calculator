18746f96fe948b30fd821da8c7b5a6d64c77501c
ali-qattoush <aliqattoushwork@gmail.com>
2023-06-24 15:52:17 +0300

    calculator added, icons removed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { evaluate } from 'mathjs';
+import "./App.css";
 
 function App() {
+  const [displayValue, setDisplayValue] = useState("");
+
+  const handleButtonClick = (value) => {
+    setDisplayValue(displayValue + value);
+  };
+
+  const handleReset = () => {
+    setDisplayValue("");
+  };
+
+  const calculate = () => {
+    const result = evaluate(displayValue);
+    setDisplayValue(result.toString());
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" value={displayValue} readOnly />
+      <br></br>
+      <button onClick={() => handleButtonClick(7)}>7</button>
+      <button onClick={() => handleButtonClick(8)}>8</button>
+      <button onClick={() => handleButtonClick(9)}>9</button>
+      <button onClick={() => handleButtonClick("/")}>/</button>
+      <br></br>
+      <button onClick={() => handleButtonClick(4)}>4</button>
+      <button onClick={() => handleButtonClick(5)}>5</button>
+      <button onClick={() => handleButtonClick(6)}>6</button>
+      <button onClick={() => handleButtonClick("*")}>*</button>
+      <br></br>
+      <button onClick={() => handleButtonClick(1)}>1</button>
+      <button onClick={() => handleButtonClick(2)}>2</button>
+      <button onClick={() => handleButtonClick(3)}>3</button>
+      <button onClick={() => handleButtonClick("-")}>-</button>
+      <br></br>
+      <button onClick={() => handleButtonClick(0)}>0</button>
+      <button onClick={() => handleButtonClick(".")}>.</button>
+      <button onClick={calculate}>=</button>
+      <button onClick={() => handleButtonClick("+")}>+</button>
+      <br></br>
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
 }
-
-console.log("git test")
 
 export default App;
